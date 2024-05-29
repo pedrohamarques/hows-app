98c7e16e28cc1aeb2d303bc8b7d08b78969be959
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-05-29 11:09:42 -0300

    feat: add custom header and settings screen to stack

--- src/routes/private-routes.tsx
+++ src/routes/private-routes.tsx
@@ -2,14 +2,35 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import HomeScreen from "@screens/home";
 
+import { CustomHeader } from "@components/header-custom";
+
 import { PRIVATE_ROUTES, PrivateRoutesParams } from "@typings/routes";
+import SettingsScreen from "@screens/settings";
 
 export default function PrivateRoutes() {
   const Stack = createNativeStackNavigator<PrivateRoutesParams>();
 
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name={PRIVATE_ROUTES.HOME} component={HomeScreen} />
+    <Stack.Navigator>
+      <Stack.Screen
+        name={PRIVATE_ROUTES.HOME}
+        component={HomeScreen}
+        options={{
+          header: () => (
+            <CustomHeader
+              title="Chats"
+              testID="routes.private-routes.home-screen"
+            />
+          ),
+        }}
+      />
+      <Stack.Screen
+        name={PRIVATE_ROUTES.SETTINGS}
+        component={SettingsScreen}
+        options={{
+          header: () => <CustomHeader title="Settings" hasAvatar={false} />,
+        }}
+      />
     </Stack.Navigator>
   );
 }
