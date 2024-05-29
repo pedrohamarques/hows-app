8ab4a312253c7aa815288e2a204d6213c85d63b3
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-05-29 11:44:28 -0300

    feat: create divider

---
+++ src/components/divider/divider.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+
+type DividerProps = {
+  testID?: string;
+};
+
+export function Divider({ testID }: DividerProps) {
+  return (
+    <View className="border-b border-b-neutral-200 mx-2" testID={testID} />
+  );
+}
