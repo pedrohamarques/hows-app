9f90968d75afc563b5ba33d020d903bb0f618c20
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-06-17 17:40:19 -0300

    chore: turn off react in jsx scope eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,10 +3,16 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
-
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  {
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  },
 ];
